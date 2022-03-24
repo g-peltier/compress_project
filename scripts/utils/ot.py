ebf6ad94583dd7b2b46b2c8aae7045d789ac57cc
Vladimir Kondratyev <kondratyev.w.i@gmail.com>
2022-03-25 00:11:14 +0100

    Final Copy

--- scripts/utils/ot.py
+++ scripts/utils/ot.py
@@ -4,11 +4,44 @@ import matplotlib.pyplot as plt
 from IPython.display import clear_output
 
 def get_cost(X, Y, A):
-    """Given two sets of vectors, returns the cost of transport between them"""
+    """
+    Given two sets of vectors, returns the cost of transport between them
+
+    Parameters
+    ----------
+    X : np.array, dims = (Batch_Size, N_x)
+        The array of X vectors for matching problem
+        
+    Y : np.array, dims = (Batch_Size, N_y)
+        The array of Y vectors for matching problem
+        
+    A : np.array, dims = (N_x, N_y)
+        The matrix to specify the cost function
+
+    """
+    
     return X@A@Y.T
 
 def Sinkhorn(mu1, mu2, Phi, sigma=0.1, max_iter=100):
-    """The Sinkhorn algorithm to compute a, b functions"""
+    """
+    The Sinkhorn algorithm to compute a, b functions
+    
+    Parameters
+    ----------
+    mu1 : np.array, dims = (N_x)
+        Marginal distribution of x's
+        
+    mu2 : np.array, dims = (N_y)
+        Marginal distribution of y's
+        
+    sigma : float, optional
+        The entropy regulerization strength (default is 0.1)
+        
+    max_iter : int, optional
+        Number of cycles of Sinkhorn to perform (default is 100)
+
+    """
+    
     b = np.ones_like(mu2)
     K = np.exp(Phi/sigma)
     for _ in range(max_iter):
@@ -18,11 +51,62 @@ def Sinkhorn(mu1, mu2, Phi, sigma=0.1, max_iter=100):
     return a, b
 
 def get_distr(a, b, Phi, sigma=0.1):
-    """Given a, b functions retunrs the distribution of mass"""
+    """
+    Given a, b functions retunrs the distribution of mass
+    
+    Parameters
+    ----------
+    a : np.array, dims = (N_x)
+        Vector a computed by Sinkhorn
+        
+    b : np.array, dims = (N_y)
+        Vector b computed by Sinkhorn
+        
+    sigma : float, optional
+        The entropy regulerization strength (default is 0.1)
+        
+    """
+    
     return ((np.diag(a).dot(np.exp(Phi/sigma))).dot(np.diag(b)))
 
 def proximal_descent(A, X, mu1, Y, mu2, t=0.1, l=0.1, sigma=0.1, n_steps=100, verbose=True, svd_flag=False):
-    """Gradient descent, to estimate the matrix A, given optimal matching"""
+    """
+    Gradient descent, to estimate the matrix A, given optimal matching
+        
+    Parameters
+    ----------
+    A : np.array, dims = (N_x, N_y)
+        The matrix to specify the cost function
+        
+    X : np.array, dims = (Batch_Size, N_x)
+        The array of X vectors for matching problem
+    
+    mu1 : np.array, dims = (N_x)
+        Marginal distribution of x's
+        
+    Y : np.array, dims = (Batch_Size, N_y)
+        The array of Y vectors for matching problem
+        
+    mu2 : np.array, dims = (N_y)
+        Marginal distribution of y's
+        
+    t : float, optional
+        Size step of graident descent (default is 0.1)
+        
+    sigma : float, optional
+        The entropy regulerization strength (default is 0.1)
+    
+    n_steps : int, optional
+        Number of cycles of Gradient Descent to perform (default is 100)
+        
+    verbose : bool, optional
+        Wether to plot the training proccess or not (default is True)
+        
+    svd_flag : bool, optional
+        Wether to use nuclear norm regulerization (default is False)
+        
+    """
+    
     X_size = X.shape[0]
     Y_size = Y.shape[0]
     size = min(X_size, Y_size)
